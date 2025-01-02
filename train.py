b9438fcd0fcaed39d94bdbb3cd950bb303b82213
FabianKowatsch <f.kowatsch2000@gmail.com>
2025-01-02 12:33:01 +0100

    fixed training and added new model

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ from modules.detection import LPD_Module
 
 def train():
     detector = LPD_Module("checkpoints/pretrained/yolo11n.pt")
-    results = detector.model.train(data="config/data.yaml", epochs=400, imgsz=640, device=0, val=True, single_cls=True, batch=32)
-    #results = detector.model.train(trainer="config/train.yaml")
+    #results = detector.model.train(data="config/data.yaml", epochs=400, imgsz=640, device=0, val=True, single_cls=True, batch=32)
+    results = detector.model.train(cfg="config/train.yaml")
     #print(results)
 if __name__ == "__main__":
     train()
