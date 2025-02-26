b719abe02a17399700b8bb95d017638bce813457
FabianKowatsch <f.kowatsch2000@gmail.com>
2025-02-26 15:55:46 +0100

    enabled testing

--- test.py
+++ test.py
@@ -10,61 +10,70 @@ from modules.ocr import OCR_Module
 from modules.upscaling import Upscaler
 from modules.image_processing import Processing
 
-def load_images(directory_path):
+def load_images_and_labels(directory_path):
     image_list = []
+
+    # Load images
     for filename in os.listdir(directory_path):
-        if filename == "B_JF_185.jpg":
-        #if filename.endswith('.jpg'):
+        if filename.endswith('.jpg'):
             image_path = os.path.join(directory_path, filename)
             img = cv2.imread(image_path)
             if img is not None:
-                image_list.append(img)
-    #images = torch.tensor(np.stack(image_list, axis=0))
-    #images = images.permute(0, 3, 1, 2)
+                # Match the image filename (without extension) to its label
+                image_name = os.path.splitext(filename)[0]
+                #correct_label = labels.get(image_name, "Unknown")
+                image_list.append((img, image_name))
+                print(image_name)
+
     return image_list
 
 def test(config):
-
-    images = load_images(config["data_path"])
+    images = load_images_and_labels(config["data_path"])
     detector = LPD_Module(config["lpd_checkpoint_path"])
     ocr = OCR_Module(config["recognizer"])
     upscaler = Upscaler(config["upscaler"])
     image_processing = Processing(config["image_processing"])
-    visualize = config["visualize"]
 
-    if(visualize):
-        #plt.ion()
-        _, axes = plt.subplots(2, 1)
+    for i, image_and_label in enumerate(images):
+        image = image_and_label[0]
+        try:
+            boxes = detector(image)
+            if not boxes:  # No license plates detected
+                continue
+        except Exception as e:
+            continue
 
-    for image in images:
+        for j, box in enumerate(boxes):
+            try:
+                # Crop the image to simplify ocr
+                lp_image = crop_image(image, box)
 
-        # LP detection
-        boxes = detector(image)
+                # Save the cropped image
+                cropped_image_path = f'static/uploads/cropped_plate_{i}_{j}.png'
+                cv2.imwrite(cropped_image_path, lp_image)
 
-        for box in boxes:
+                # Upscaling
+                lp_image = upscaler(lp_image)
 
-            # Crop the image to simplify ocr
-            lp_image = crop_image(image, box)
+                # Processing
+                lp_image = image_processing(lp_image)
 
-            # Upscaling
-            lp_image = upscaler(lp_image)
+                # Text recognition
+                lp_text = ocr(lp_image)
 
-            # Processing
-            lp_image = image_processing(lp_image)
+                # Filter text, replace some symbols with spaces
+                text_filtered =  re.sub(r'(?<!\s)[^A-Z0-9-\s](?!\s)', ' ', lp_text)
 
-            # Text recognition
-            lp_text = ocr(lp_image)
+                gt =image_and_label[1]
 
-            # Filter text, replace some symbols with spaces
-            text_filtered =  re.sub(r'(?<!\s)[^A-Z0-9-\s](?!\s)', ' ', lp_text)
+                print("Predicted: ", text_filtered)
+                print("GT: ", gt)
+                print("Correct: ", gt == text_filtered)
+                print("")
 
-            if(visualize):
-                show_image(image, lp_image, text_filtered, box, axes)
-
-    if(visualize):      
-        #plt.ioff()
-        #plt.show()
-        pass
+            except Exception as e:
+                print(e)
+                continue
 
 def crop_image(image, box):
     x1, y1, x2, y2 = map(int, box.xyxy[0])
