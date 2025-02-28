0687928a1b791d737fbf61c854b8a00af82d8bbb
FabianKowatsch <f.kowatsch2000@gmail.com>
2025-02-28 17:07:20 +0100

    Overwriting main

--- modules/detection.py
+++ modules/detection.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 class LPD_Module:
     def __init__(self, model_path, verbose=False):
         self.model = YOLO(model_path, verbose=verbose)
-
+ 
     def __call__(self, images: torch.Tensor):
         results = self.model(source=images)
         boxes = []
